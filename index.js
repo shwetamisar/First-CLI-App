06bb7ca59d17bc9fa375fec6474eae28a01c350a
shwetamisar <62926371+shwetamisar@users.noreply.github.com>
2023-01-05 12:51:25 +0000

    Added options

--- index.js
+++ index.js
@@ -1,54 +1,94 @@
 //read input from user
 var readlineSync = require('readline-sync');
-//take username
-var userName = readlineSync.question("What is your name? ");
-console.log("welcome " + userName + " to do you know shweta?");
+console.log("--------------------------------------");
+console.log("Let's see. How well do you know me?");
+console.log("--------------------------------------");
+
+var userName = readlineSync.question("\nWhat is your name ? ");
+console.log("\nWelcome " + userName + " \nLet's get started!!")
 //count score 
-var countScore = 0;
+var countScore = 0
+
 //check if user answer is valid or not
-function checkAnswer(question, answer) {
-  var userAnswer = readlineSync.question(question);
+function checkAnswer(question, optionA, optionB, optionC, optionD, answer) {
+  console.log("\nselect ONE OPTION from A,B,C,D");
+  console.log(question + "\nA: " + optionA + "\nB: " + optionB + "\nC: " + optionC + "\nD: " + optionD);
+
+
+  var userAnswer = readlineSync.question("Your answer: ");
   if (userAnswer.toUpperCase() == answer.toUpperCase()) {
-    console.log("Kudos to you!! ");
+    console.log("\nKudos to you!! ");
     countScore++;
     console.log("Current score: " + countScore);
   }
   else
-    console.log("Better luck next time..");
-  console.log("---------------------");
+    console.log("\nBetter luck next time..");
+
+  console.log("------------------------------------");
+
 }
 
-//arrray of objects
-var questionList = [{
-  question: "what is my pet name?",
-  answer: "radha",
-}, {
-  question: "where do i live?",
-  answer: "nagpur",
-}, {
-  question: "who is my favourite actor?",
-  answer: "manoj bajpayee",
-}, {
-  question: "where do I work?",
-  answer: "infosys",
-}, {
-  question: "what is favorite dish?",
-  answer: "puranpoli",
-}];
-//storing data of high scorers
+
+//store question and answer in array
+var questionList = [
+  {
+    question: "\nwhat is my nick name? ",
+    optionA: "Shreya",
+    optionB: "Radha",
+    optionC: "Pacha",
+    optionD: "Ishu",
+    answer: "B"
+  },
+  {
+    question: "\nwhere do I live? ",
+    optionA: "Nashik",
+    optionB: "Pune",
+    optionC: "Aurangabad",
+    optionD: "Nagpur",
+    answer: "D"
+  },
+  {
+    question: "\nwho is my favourite actor?",
+    optionA: "Manoj Bajpayee",
+    optionB: "Pankaj Tripathi",
+    optionC: "Raj Kumar Rao",
+    optionD: "Salman Khan",
+    answer: "A"
+  },
+  {
+    question: "\nwhere do I work? ",
+    optionA: "Infosys",
+    optionB: "Infocepts",
+    optionC: "Capgemini",
+    optionD: "Codidas",
+    answer: "A"
+  },
+
+  {
+    question: "\nwhat is favorite dish?",
+    optionA: "Gulab Jamun",
+    optionB: "Rasgulla",
+    optionC: "Puranpoli",
+    optionD: "Basundi",
+    answer: "C"
+  }
+];
+//track of high score
 var highScore = [{
   Name: "Shweta",
   score: 5
 }, {
-  Name: "Vaishnavi",
-  score: 4
+  Name: "Ishwari",
+  score: 3
 }]
 
 //call function using each object of array
 for (var i = 0; i < questionList.length; i++) {
-  checkAnswer(questionList[i].question, questionList[i].answer);
+  checkAnswer(questionList[i].question, questionList[i].optionA, questionList[i].optionB, questionList[i].optionC, questionList[i].optionD, questionList[i].answer);
 }
+
 //total score
+
 console.log("\nHey..You SCORED  " + countScore);
 console.log("---------------------");
 //Name of high scorers
@@ -58,5 +98,3 @@ for (var i = 0; i < highScore.length; i++) {
 }
 console.log("---------------------");
 console.log("Remember to send me screenshot if you beat the score..\n I will update your name in high scorer list...")
-
-
